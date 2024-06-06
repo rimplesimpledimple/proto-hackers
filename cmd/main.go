cc7f5fd06671fbd23c4de9e49d42d583b9a6bd77
Rimple <rimple@rimple.local>
2024-06-06 15:11:30 +0530

    Smoke Test

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"protohackers"
+)
+
+func main() {
+	nogptallowed.SmokeTest()
+}
---
+++ smoketest.go
@@ -0,0 +1,31 @@
+package nogptallowed
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+func SmokeTest() {
+	listener, err := net.ListenTCP("tcp", &net.TCPAddr{IP: net.IPv4(0, 0, 0, 0), Port: 47777})
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := listener.AcceptTCP()
+		if err != nil {
+			panic(err)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	_, err := io.Copy(conn, conn)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
